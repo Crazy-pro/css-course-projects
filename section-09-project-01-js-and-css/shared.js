fc6d8ca8db92dc556d8a242b5872929e62e4c08d
Crazy-pro <Crazy.pro.noob@gmail.com>
2024-02-09 10:00:00 +0000

    refactor to classList add and remove methods

--- section-09-project-01-js-and-css/shared.js
+++ section-09-project-01-js-and-css/shared.js
@@ -7,24 +7,24 @@ const mobileNav = document.querySelector('.mobile-nav')
 
 for (const i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
-        modal.style.display = 'block'
-        backdrop.style.display = 'block'
+        backdrop.classList.add('open')
+        modal.classList.add('open')
     })
 }
 
 backdrop.addEventListener('click', function () {
-    mobileNav.style.display = 'block'
+    mobileNav.classList.remove('open')
     closeModal()
 })
 
 modalNoButton.addEventListener('click', closeModal)
 
 function closeModal() {
-    backdrop.style.display = 'none'
-    modal.style.display = 'none'
+    backdrop.classList.remove('open')
+    modal.classList.remove('open')
 }
 
 toggleButton.addEventListener('click', function () {
-    backdrop.style.display = 'none'
-    mobileNav.style.display = 'block'
+    backdrop.classList.add('open')
+    mobileNav.classList.add('open')
 })
